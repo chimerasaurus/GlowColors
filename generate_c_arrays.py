dc51fee82f74b873c220ec6f452450d20b442a5f
James Malone <jamalone@gmail.com>
2013-03-22 23:07:47 -0700

    Removed left generation from generate arrays

--- generate_c_arrays.py
+++ generate_c_arrays.py
@@ -22,9 +22,6 @@ def main():
     # Generate right color arrays
     generate_color_arrays('RIGHT')
     
-    # Generate left color arrays
-    generate_color_arrays('LEFT')
-    
 # Translate a python array into a string for Arduino
 def array_to_string(array):
     array_string = ','.join(str(x) for x in array)
@@ -35,21 +32,10 @@ def generate_color_arrays(side):
     # Array to hold encoded data
     encoded_data = []
     
-    if (side == 'RIGHT' or side == 'BOTH'):
-        # Print right and both arrays
-        for color in glowcolors.settings.COLORS[side]:
-            message = glowcolors.message.generate(color)
-            encoded_data = glowcolors.encode.ir_encode(message, True)
-            print "int %s_%s[%s] = %s" % (side, color, len(encoded_data), array_to_string(encoded_data))
-    else:
-        # Print left arrays
-        right_off_data = glowcolors.encode.ir_encode(glowcolors.message.generate('INACTIVE', 'RIGHT'))
-        right_inactive_encoded_data = glowcolors.encode.ir_encode(right_off_data, True)
-        for color in glowcolors.settings.COLORS['BOTH']:
-            message = glowcolors.message.generate(color)
-            encoded_data = glowcolors.encode.ir_encode(message, True)
-            encoded_data += right_off_data
-            print "const int %s_%s[%s] = %s" % (side, color, len(encoded_data), array_to_string(encoded_data))
+    for color in glowcolors.settings.COLORS[side]:
+        message = glowcolors.message.generate(color)
+        encoded_data = glowcolors.encode.ir_encode(message, True)
+        print "int %s_%s[%s] = %s" % (side, color, len(encoded_data), array_to_string(encoded_data))
     
 # Helpers
 ## Run the main() function on execution
